9410b987526f4bd957623946573523b52afd6e81
Thierno-hub <thiernobah0w@gmail.com>
2024-10-25 16:32:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var videoSource = document.getElementById("video-source");
 function jour () {
     (body.classList.toggle("night")) 
         if (body.classList.contains("night")) {
-            videoSource.src = "./gokussj.mp4";
+            videoSource.src = "./gokudark.mp4";
         } else {
-            videoSource.src="./gokudark.mp4";
+            videoSource.src="./gokussj.mp4";
         }
 
         var video = document.getElementById("background-video");
